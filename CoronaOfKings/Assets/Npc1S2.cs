fc8922e5c67d164ad22dc3a2663414d245e284aa
Jędrzej <jedrzejleder@gmail.com>
2020-06-07 01:41:45 +0200

    Wiecej plansz gry

---
+++ CoronaOfKings/Assets/Npc1S2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc1S2 : MonoBehaviour
+{
+    public static int nr;
+    public Rigidbody2D lb;
+    public Animator anim = new Animator();
+    //bool xd = Move.xd;
+    void Start()
+    {
+        anim.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        nr = 1;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        nr = 0;
+    }
+    public void ExitRightAnswer()
+    {
+        anim.SetBool("tuptani2", true);
+        transform.localScale = new Vector2(-1, 1);
+        lb.velocity = new Vector2(2, 0);
+    }
+}
