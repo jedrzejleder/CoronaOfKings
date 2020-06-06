f080cb9d04705fc8f5d634dd3de0fd963a342a51
Rogalsonn <kuba1b14@gmail.com>
2020-06-06 19:22:02 +0200

    dodany dymek dla człowieczka który mówi, ze da radę
    da radę

--- CoronaOfKings/Assets/champmovement.cs
+++ CoronaOfKings/Assets/champmovement.cs
@@ -7,38 +7,32 @@ public class champmovement : MonoBehaviour
     Animator anim;
     // Start is called before the first frame update
     void Start()
-    {
+    { 
+   
         anim = GetComponent<Animator>();
+        
     }
     public float speed = 1.5f;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-         {
+
+        if (!Input.GetKey(KeyCode.RightArrow))
+        {
             transform.position += Vector3.right * speed * Time.deltaTime;
-            if(anim!= null)
+            if (anim != null)
             {
                 anim.Play("walk");
-                
+
             }
 
         }
         else
         {
             anim.Play("idlemainchar");
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+
         }
     }
+
+
 }
