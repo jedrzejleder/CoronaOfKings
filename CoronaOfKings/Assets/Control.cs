8cb220e8ddcb886f33fb1c2ab300e71f404fab5f
Łukasz <lukasz.chandzynski@gmail.com>
2020-06-06 22:03:44 +0200

    Version 0.5

---
+++ CoronaOfKings/Assets/Control.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator anim;
+    public int health;
+    public void Start()
+    {
+     health = 17;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        enemy badguy = collision.gameObject.GetComponent<enemy>();
+        if(collision.gameObject.tag == "Enemy")
+        {
+            anim.SetBool("running", false);
+            Attack();
+            health--;
+            if (health != 0)
+            {
+                badguy.Block();
+            }
+            else
+            {
+                badguy.Killed();
+            }
+        }
+    }
+    private void Update()
+    {
+            rb.velocity = new Vector2(4, 0);
+            transform.localScale = new Vector2(1, 1);
+            anim.SetBool("running", true);
+    }
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        
+    }
+    private void poof()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ CoronaOfKings/Assets/enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public Animator anim2;
+    public Rigidbody2D lb;
+    void Start()
+    {
+        
+    }
+
+    public void Killed()
+    {
+        anim2.SetTrigger("dead");
+    }
+    public void Block()
+    {
+        anim2.SetTrigger("block");
+    }
+
+    private void Death()
+    {
+        Destroy(this.gameObject);
+    }
+}
