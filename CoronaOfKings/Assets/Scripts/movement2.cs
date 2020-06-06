9d66b014c78dffab8c04d0441d68385940bed12a
Rogalsonn <kuba1b14@gmail.com>
2020-06-07 00:08:55 +0200

    two final scenes that should work
    should

---
+++ CoronaOfKings/Assets/Scripts/movement2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement2 : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+    public float speed = 1.5f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x<-9)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            if (anim != null)
+            {
+                anim.Play("walk");
+
+            }
+
+        }
+        else
+        {
+            anim.Play("idlemainchar");
+
+        }
+    }
+
+
+}
